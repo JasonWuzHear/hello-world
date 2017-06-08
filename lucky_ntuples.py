6b2baf917814e1b5d1e4a950f2e051b4dcfd9511
Jason BouKheir <jason.boukheir@gmail.com>
2017-06-08 18:27:48 -0500

    added in an interesting program...

---
+++ lucky_ntuples.py
@@ -0,0 +1,61 @@
+dic = {}
+lucky = 0
+
+def isqrt(n):
+    x = n
+    y = (x + 1) // 2
+    while y < x:
+        x = y
+        y = (x + n // x) // 2
+    return x
+
+def answer(l, dmax):
+    global lucky
+    lucky = dmax
+    global dic
+    dic = {}
+
+    count = 0
+    for i in range(len(l)):
+        if str(l[i]) not in dic:
+           dic[str(l[i])] = 0
+        dic[str(l[i])] += 1
+
+        try:
+            if l[i] == l[i+1]:
+                continue
+            count += div(l[i], lucky)
+        except IndexError:
+            count += div(l[i], lucky)
+
+    return count
+
+def div(n, d):
+    global dic
+    dic[str(n)] -= 1
+
+    if dic[str(n)] < 0:
+        dic[str(n)] += 1
+        return 0
+    elif d == 1:
+        dic[str(n)] += 1
+        return 1 
+    elif str(n) + ',' + str(d) in dic:
+        if d == lucky:
+            dic[str(n) + ',' + str(d)] *= -1
+        else:
+            dic[str(n)] += 1
+            return dic[str(n) + ',' + str(d)]
+    else:
+        dic[str(n) + ',' + str(d)] = 0
+
+    sqrt = isqrt(n)
+    for i in range(1, sqrt+1):
+        if n%i == 0:
+            if str(i) in dic:
+                dic[str(n) + ',' + str(d)] += div(i, d-1)
+            if (i**2 != n) and (str(n//i) in dic):
+                dic[str(n) + ',' + str(d)] += div(n//i, d-1)
+
+    dic[str(n)] += 1    
+    return dic[str(n) + ',' + str(d)]
